e7636d50576e8dfc0d65dd7cd478fc5d71270a43
Jones <jlindb18@student.aau.dk>
2024-02-08 11:25:19 +0100

    updated test

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -6,8 +6,7 @@ def test_identify_relationships():
     functions = [('add', ['x', 'y'], ['x + y']), ('subtract', ['x', 'y'], ['x - y'])]
     relationships = identify_relationships(functions)
     expected_relationships = {
-        'add': {'parameters': {'x', 'y'}, 'returns': {'x + y'}, 'related_functions': 
-        {'subtract'}}, 'subtract': {'parameters': {'x', 'y'}, 'returns': {'x - y'}, 'related_functions': {'add'}}
-    }
+        'add': {'parameters': {'x', 'y'}, 'returns': {'x + y'}, 'related_functions': {'subtract'}, 'param_loose_ends': {'x', 'y'}, 'returns_loose_ends': {'y'}}, 'subtract': {'parameters': {'x', 'y'}, 'returns': {'x - y'}, 'related_functions': {'add'}, 'param_loose_ends': {'x', 'y'}, 'returns_loose_ends': {'y'}}
+        }
     assert relationships == expected_relationships
     assert len(relationships) == 2
