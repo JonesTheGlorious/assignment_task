8feb4428eaec59add7bd2074b8d6809eaf8817ce
Jones <jlindb18@student.aau.dk>
2024-02-14 10:36:35 +0100

    test update

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -4,9 +4,9 @@ from src.relationship import identify_relationships
 def test_identify_relationships():
     # Test case with sample functions
     functions = [('add', ['x', 'y'], ['x + y']), ('subtract', ['x', 'y'], ['x - y'])]
-    relationships = identify_relationships(functions)
+    relationships = identify_relationships(functions, '_')
     expected_relationships = {
-        'add': {'parameters': {'x', 'y'}, 'returns': {'x + y'}, 'related_functions': {'subtract'}, 'param_loose_ends': {'x', 'y'}, 'returns_loose_ends': {'y'}}, 'subtract': {'parameters': {'x', 'y'}, 'returns': {'x - y'}, 'related_functions': {'add'}, 'param_loose_ends': {'x', 'y'}, 'returns_loose_ends': {'y'}}
+        'add': {'parameters': {'x', 'y'}, 'returns': {'x + y'}, 'related_functions': {'subtract'}, 'param_loose_ends': {'x', 'y'}, 'returns_loose_ends': {'x + y'}}, 'subtract': {'parameters': {'x', 'y'}, 'returns': {'x - y'}, 'related_functions': {'add'}, 'param_loose_ends': {'x', 'y'}, 'returns_loose_ends': {'x - y'}}
         }
     assert relationships == expected_relationships
     assert len(relationships) == 2
